23ee55f19cdfaf1924226a701fa92c7307de54d4
luckyjd5 <JD210029@reigate.ac.uk>
2022-11-18 10:23:22 +0000

    .

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module rs.cs.restaurantnea {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens rs.cs.restaurantnea to javafx.fxml;
+    exports rs.cs.restaurantnea;
+}
